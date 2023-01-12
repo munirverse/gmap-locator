3725362e6b2e9b66ef0ace48845c8c44e6258721
softwaremakassar <softwaremakassar@gmail.com>
2023-01-12 14:13:58 +0800

    feat(helper): add function extraArgs and validateArgs

---
+++ test/src/helper/index.test.js
@@ -0,0 +1,57 @@
+const helper = require('../../../src/helper');
+
+describe('test helper module', () => {
+  it('test function extraArgs', () => {
+    // Given
+    const exampleArgs = [null, null, '-h'];
+    const exampleArgs2 = [null, null, '-i', 'test.csv', '-o', 'output.csv'];
+    const exampleArgs3 = [
+      null,
+      null,
+      '-h',
+      '-i',
+      'test.csv',
+      '-o',
+      'output.csv'
+    ];
+
+    // When
+    const args = helper.extraArgs(exampleArgs);
+    const args2 = helper.extraArgs(exampleArgs2);
+    const args3 = helper.extraArgs(exampleArgs3);
+
+    // Then
+    expect(args).toMatchObject({ '-h': true });
+    expect(args2).toMatchObject({ '-i': 'test.csv', '-o': 'output.csv' });
+    expect(args3).toMatchObject({
+      '-h': true,
+      '-i': 'test.csv',
+      '-o': 'output.csv'
+    });
+  });
+  it('test function validateArgs', () => {
+    // Given
+    const allowedArgs = { '-i': 'string', '-o': 'string', '-h': 'boolean' };
+    const exampleArgs = { '-h': true, '-i': 'test.csv', '-o': 'output.csv' };
+    const exampleArgs2 = { '-h': 'test', '-i': true, '-o': 2 };
+    const exampleArgs3 = { '-z': true };
+    const exampleArgs4 = {
+      '-h': true,
+      '-i': 'test.csv',
+      '-o': 'output.csv',
+      '-z': true
+    };
+
+    // When
+    const result = helper.validateArgs(exampleArgs, allowedArgs);
+    const result2 = helper.validateArgs(exampleArgs2, allowedArgs);
+    const result3 = helper.validateArgs(exampleArgs3, allowedArgs);
+    const result4 = helper.validateArgs(exampleArgs4, allowedArgs);
+
+    // Then
+    expect(result).toBe(true);
+    expect(result2).toBe(false);
+    expect(result3).toBe(false);
+    expect(result4).toBe(false);
+  });
+});
